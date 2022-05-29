e78af25feea316aa8eb7b7e5c51980544a93e29f
dogukanakkaya <doguakkaya27@hotmail.com>
2022-05-29 13:59:08 +0300

    revert: image tag not works with static builds

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image'
-
 function Footer() {
     return (
         <div className='flex items-center justify-center h-20 border-t dark:border-gray-800'>
-            <Image src="/logo.png" alt="Permahistory" width={35} height={35} className='cursor-pointer' />
+            <img className='w-8' src="/logo.png" alt="Permahistory" />
         </div>
     )
 }
