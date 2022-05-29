23227fd473ecbbd0451e7cf82ad97e1eefd65319
dogukanakkaya <doguakkaya27@hotmail.com>
2022-05-29 15:06:38 +0300

    feat: loading

--- pages/history.tsx
+++ pages/history.tsx
@@ -3,13 +3,17 @@ import config from '../config';
 import { arweave } from '../utils';
 import HistoryItem, { type HistoryItem as HistoryItemType } from '../components/history-item';
 import Head from 'next/head';
+import Loading from '../components/loading';
 
 function History() {
     const [loading, setLoading] = useState(false);
     const [history, setHistory] = useState<HistoryItemType[]>([]);
 
     const getTransactions = async () => {
-        setLoading(true);
+        const loadingTimeout = setTimeout(() => {
+            setLoading(true);
+        }, 250);
+
         try {
             const response = await fetch(`${config.ARWEAVE_URL}/graphql`, {
                 method: 'POST',
@@ -50,17 +54,16 @@ function History() {
 
             // todo: find a better way to do this (maybe saving txId when making transaction)
             setHistory(txDatas.map(txData => {
-                console.log(txData);
-
                 const d = JSON.parse(txData);
                 d.txId = txIds[txDatas.indexOf(txData)];
 
                 return d;
             }));
+
+            clearTimeout(loadingTimeout);
         } catch (err) {
             console.log(err);
         }
-
         setLoading(false);
     }
 
@@ -76,13 +79,22 @@ function History() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <div className="min-h-[calc(100vh-theme('spacing.40'))] container my-10">
-                <div className="grid lg:grid-cols-2 gap-4">
-                    {
-                        history.map((h, i) => <HistoryItem key={i} item={h} />)
-                    }
-                </div>
-            </div>
+            {
+                loading ? (
+                    <div className="h-[calc(100vh-theme('spacing.20'))] flex items-center justify-center">
+                        <Loading />
+                    </div>
+                ) : (
+                    <div className="min-h-[calc(100vh-theme('spacing.40'))] container my-10">
+                        <div className="grid lg:grid-cols-2 gap-4">
+                            {
+                                history.map((h, i) => <HistoryItem key={i} item={h} />)
+                            }
+                        </div>
+                    </div>
+                )
+            }
+
         </>
     )
 }
