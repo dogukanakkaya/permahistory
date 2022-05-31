c421cbab47a06077c444dd0996be596d4e58ca49
dogukanakkaya <doguakkaya27@hotmail.com>
2022-06-01 01:54:58 +0300

    fix: deploy config

--- next.config.js
+++ next.config.js
@@ -9,6 +9,9 @@ const nextConfig = {
 if (process.env.NODE_ENV === 'static_export') {
     nextConfig.assetPrefix = 'http://127.0.0.1:5500/out';
     nextConfig.basePath = '/out';
+} else if (process.env.NODE_ENV === 'production') {
+    nextConfig.assetPrefix = '.';
+    nextConfig.basePath = '/out';
 }
 
 module.exports = {
