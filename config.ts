80b8d7950c41a13c8719d00796d965b736abff67
dogukanakkaya <doguakkaya27@hotmail.com>
2022-05-31 22:56:03 +0300

    for deployment

--- config.ts
+++ config.ts
@@ -4,14 +4,14 @@ const config = {
         algorithm: "RSA-OAEP",
         hash: "SHA-256",
     },
-    ARWEAVE_HOST: '127.0.0.1',
-    ARWEAVE_PORT: 1984,
-    ARWEAVE_PROTOCOL: 'http',
-    ARWEAVE_URL: 'http://127.0.0.1:1984'
-    // ARWEAVE_HOST: 'arweave.net',
-    // ARWEAVE_PORT: 443,
-    // ARWEAVE_PROTOCOL: 'https',
-    // ARWEAVE_URL: 'https://arweave.net:443'
+    // ARWEAVE_HOST: '127.0.0.1',
+    // ARWEAVE_PORT: 1984,
+    // ARWEAVE_PROTOCOL: 'http',
+    // ARWEAVE_URL: 'http://127.0.0.1:1984'
+    ARWEAVE_HOST: 'arweave.net',
+    ARWEAVE_PORT: 443,
+    ARWEAVE_PROTOCOL: 'https',
+    ARWEAVE_URL: 'https://arweave.net:443'
 };
 
 export default config;
--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     trailingSlash: true
 };
 
-if (process.env.NODE_ENV === 'test') {
+if (process.env.NODE_ENV === 'static_export') {
     nextConfig.assetPrefix = 'http://127.0.0.1:5500/out';
     nextConfig.basePath = '/out';
 }
