d5ffaf89f8da0286d96d74b64c582b45950f0d88
dogukanakkaya <doguakkaya27@hotmail.com>
2022-06-01 21:43:11 +0300

    fix: remove asset prefix for prod

--- next.config.js
+++ next.config.js
@@ -9,8 +9,6 @@ const nextConfig = {
 if (process.env.NODE_ENV === 'static_export') {
     nextConfig.assetPrefix = 'http://127.0.0.1:5500/out';
     nextConfig.basePath = '/out';
-} else if (process.env.NODE_ENV === 'production') {
-    nextConfig.assetPrefix = '.';
 }
 
 module.exports = {
