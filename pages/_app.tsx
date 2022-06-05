0dd604802606d0f67e287772058882ad067a5cc8
dogukanakkaya <doguakkaya27@hotmail.com>
2022-06-05 15:21:55 +0300

    fix: favicon src

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,14 @@ import type { AppProps } from 'next/app';
 import Layout from '../components/layout';
 import Head from 'next/head';
 import { ArConnectProvider } from '../context/useArConnect';
+import favicon from '../public/favicon.ico';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ArConnectProvider>
       <Layout>
         <Head>
-          <link rel="icon" href="./favicon.ico" />
+          <link rel="icon" href={favicon.src} />
         </Head>
         <Component {...pageProps} />
       </Layout>
